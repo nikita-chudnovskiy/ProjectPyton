cd08bd236f0ddd3c4c35653c9d1a906bce0fd35e
n.chudnovskiy <nikita.chudnovskiy@mail.ru>
2019-11-13 14:57:37 +0300

    add

--- Lesson06_While_.py
+++ Lesson06_While_.py
@@ -1,8 +1,9 @@
 x = int(input())
-while x <= 10:         # Чтобы прервать цикл while досрочно исп инструкцию break
-	print(x)
-	x += 1
-	if x > 5:
-		print('Print Breaking')
-		break
-print('Finish')
+while x <= 10:  # Чтобы прервать цикл while досрочно исп инструкцию break
+    print(x)
+    x += 1
+    if x >= 10:
+        print("Вышли из цикла")#
+        break
+else:
+    print('Finish')
