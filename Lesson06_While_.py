aa2ae900a8a225fe724704716570439cd3f7e2fe
n.chudnovskiy <nikita.chudnovskiy@mail.ru>
2019-11-14 15:59:36 +0300

    add

--- Lesson06_While_.py
+++ Lesson06_While_.py
@@ -1,7 +1,8 @@
-for x  in range(1,100,1): # start stop шаг
-    if x <5 :
-        print(x, 'Дошли до 5') # доходим до 5 продолжаем
-        continue
-    print(x)
-    if x >= 10: # доходим до 8 выходим
-        break
+i = 0
+while i <= 10:
+    i+=1
+    print(i +2)
+    if i > 0:
+        print('   ',i+1)
+
+
