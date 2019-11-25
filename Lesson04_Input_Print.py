8ca0891fe76ea71f1449e96b8b815a27bf437844
n.chudnovskiy <nikita.chudnovskiy@mail.ru>
2019-11-25 23:04:17 +0300

    add

---
+++ Lesson04_Input_Print.py
@@ -0,0 +1,14 @@
+"""a = input() #str
+b = int(input())# int
+c = float(input()) # float
+"""
+a = float(input('Введите значение a: '))
+b = float(input('Введите значение b: '))
+c = float(input('Введите значение c: '))
+p = a+b+c
+print(p)
+
+a,b = map(int,input().split())
+print(a+b)
+
+
---
+++ venv/test/__init__.py
@@ -0,0 +1,14 @@
+def hello ():
+    print('Введите сколько Вам лет ')
+    running = True
+while running:
+    age = int(input())
+    if age >= 18 and age <= 27:
+        print('Ты в армии Сынок ')
+        running = False
+    elif age < 18:
+        print('Мал еще')
+    else:
+        print('Иди за военником ')
+else:
+        print('Служу Росии')
