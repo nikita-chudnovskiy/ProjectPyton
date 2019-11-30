5aad19f53023bbd5796e957377508873d9053d97
n.chudnovskiy <nikita.chudnovskiy@mail.ru>
2019-11-30 13:34:29 +0300

    add

--- Lesson04_Input_Print.py
+++ Lesson04_Input_Print.py
@@ -1,11 +1,9 @@
-"""a = input() #str
-b = int(input())# int
+a = input()         #str
+b = int(input())    # int
 c = float(input()) # float
-"""
-a = float(input('Введите значение a: '))
-b = float(input('Введите значение b: '))
-c = float(input('Введите значение c: '))
-p = a+b+c
+
+
+p = int(a)+b+c
 print(p)
 
 a,b = map(int,input().split())
