83bd9f8934e451af5d065f361c1d148a36d236b8
n.chudnovskiy <nikita.chudnovskiy@mail.ru>
2019-11-30 18:26:20 +0300

    add

--- Lesson04_Input_Print.py
+++ Lesson04_Input_Print.py
@@ -1,18 +1,20 @@
-a = input()         #str
-b = int(input())    # int
-c = float(input()) # float
+a = input()  # str
+b = int(input())  # int
+c = float(input())  # float
 
+print(int(a) + b + int(c))
 
-p = int(a)+b+c
-print(p)
+a, b = map(int, input().split())
+print(a + b)
 
-a,b = map(int,input().split())
-print(a+b)
+print(1, 2, 3, sep=',')
+print(4, 5, sep='???', end=', ')
+print(6, sep='123')  # Нужно как минимум 2 значения
 
-name = 'Андрей'
-age = 2
-print('Исполнилось %s %s'%(name,age))
+rub = 10
+kop = 99
+print('У меня есть %s рублей %s копеек' % (rub, kop))
+print('У меня есть {} рублей'.format(rub))
 
-print(1,2, sep=',', end=',')
-print(3)
-
+rub,kop = map(int,input().split())
+print('Нам дали {} рублей {} копеек'.format(rub,kop))
--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-print(17,58,sep=':')
+# N школьников делят K яблок поровну, неделящийся остаток остается в корзинке. Сколько яблок достанется каждому школьнику?
+skolniki =int(input())
+yabloki = int(input())
+kolKagdomu = yabloki//skolniki
+kolVkorzine= yabloki%skolniki
+print('Яблок каждому',kolKagdomu,'яблок в корзине',kolVkorzine)
