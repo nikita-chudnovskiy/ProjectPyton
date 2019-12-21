13e38bd918150cfd6bb104b026b17ae129bf1c2a
n.chudnovskiy <nikita.chudnovskiy@mail.ru>
2019-12-21 17:15:56 +0300

    add

---
+++ Lesson02_2MetodyStrok.py
@@ -0,0 +1,6 @@
+a = 'Hello World'
+print(a.upper(),' Все большие')
+print(a.lower(),' Все маленькие')
+print(a)
+a = a.upper() # присвоили строке другое значение
+print(a)
---
+++ Zadachi/1.py
@@ -0,0 +1,7 @@
+n = 'Andrey'
+print(n[0:])
+print(n[-1])
+print(len(n))
+print(n[0:6])
+n = n[0:0]+'E'+n[1:]
+print(n)
