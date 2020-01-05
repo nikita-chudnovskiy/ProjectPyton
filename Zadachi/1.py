48504e411692e63e851b9c0ae2fc391835ccf156
n.chudnovskiy <nikita.chudnovskiy@mail.ru>
2020-01-05 15:09:53 +0300

    add

--- Zadachi/1.py
+++ Zadachi/1.py
@@ -1,7 +1,8 @@
-n = 'Andrey'
-print(n[0:])
-print(n[-1])
-print(len(n))
-print(n[0:6])
-n = n[0:0]+'E'+n[1:]
-print(n)
+n = 'nndrey'
+##print(n[0:])
+##print(len(n))
+#print(n[0:6])
+#n = n[0:0]+'E'+n[1:]
+
+print(n.find('n')) #
+print(n.rfind('n')) # Ищем сколько раз
