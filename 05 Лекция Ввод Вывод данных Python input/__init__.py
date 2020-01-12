1fd2c5c7edb2431c09dad00e3f4ae726eca3f7b9
n.chudnovskiy <nikita.chudnovskiy@mail.ru>
2020-01-12 18:05:20 +0300

    add

---
+++ 05 Лекция Ввод Вывод данных Python input/__init__.py
@@ -0,0 +1,28 @@
+name = (input())
+if 'Nikita' in name:
+    print('Success!')
+else:
+    print('No')
+
+
+
+a = input()  # str
+b = int(input())  # int
+c = float(input())  # float
+
+print(int(a) + b + int(c))
+
+a, b = map(int, input().split())
+print(a + b)
+
+print(1, 2, 3, sep=',')
+print(4, 5, sep='???', end=', ') # end не переводит на следующую строку 6
+print(6, sep='123')  # Нужно как минимум 2 значения
+
+rub = 10
+kop = 99
+print('У меня есть %s рублей %s копеек' % (rub, kop))
+print('У меня есть {} рублей'.format(rub))
+
+rub,kop = map(int,input().split())
+print('Нам дали {} рублей {} копеек'.format(rub,kop))
