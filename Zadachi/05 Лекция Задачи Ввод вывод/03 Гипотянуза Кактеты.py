9e972d823aebfd1f53efb27ead97dc24d9d0639b
n.chudnovskiy <nikita.chudnovskiy@mail.ru>
2020-01-13 18:30:35 +0300

    add

--- Zadachi/05 Лекция Задачи Ввод вывод/03 Гипотянуза Кактеты.py
+++ Zadachi/05 Лекция Задачи Ввод вывод/03 Гипотянуза Кактеты.py
@@ -7,3 +7,13 @@ c = pow(a,2)+pow(b,2)
 #c = c **0.5
 c = math.sqrt(c)
 print(c)
+
+# Катеты ищем a
+b,c = map(int,input().split())
+a =c**2-b**2
+print(math.sqrt(a)) # Корень из a
+
+# Катеты ищем b
+b,c = map(int,input().split())
+b =c**2-c**2
+print(math.sqrt(b)) # Корень из b
