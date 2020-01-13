27f1e4d2639ec9f9fa30d4a24bfdeeb09e7e0bb7
n.chudnovskiy <nikita.chudnovskiy@mail.ru>
2020-01-13 14:39:41 +0300

    add

--- 05 Лекция Ввод Вывод данных Python input/01 Команда input.py
+++ 05 Лекция Ввод Вывод данных Python input/01 Команда input.py
@@ -1,20 +1,30 @@
-name = (input())
+# 13.01.2020
+# Найти Периметр
+print('Найти Периметр')
+a = input('введите число')        # str Ввести строку Строку
+b = int(input('введите число'))   # int  Целое Число ВАжно !
+c = float(input('введите число')) # float Вещественное число
+perimetr=(int(a) + b + int(c)) # Преобразование типов к int
+print(perimetr)
+
+# 2 Вариант
+a,b,c= map(int,input('3 числа').split())
+perimetr = a+b+c
+print(perimetr)
+
+print(sum(map(int, input('Сумма всех введенных чисел ').split())))
+
+
+
+# Проверяем есть ли в веденной строке имя Nikita
+name = (input('Проверка имени '))
 if 'Nikita' in name:
     print('Success!')
 else:
     print('No')
 
 
-a = input('введите число')  # str
-b = int(input('введите число'))  # int
-c = float(input('введите число'))  # float
-
-print(int(a) + b + int(c))
-
-a, b = map(int, input('Введите 2 числа ').split())
-print(a + b)
-
-print(1, 2, 3, sep=',')
+print(1, 2, 3, sep=',') # разделитель запятой
 print(4, 5, sep='???', end=', ') # end не переводит на следующую строку 6
 print(6, sep='123')  # Нужно как минимум 2 значения
 
