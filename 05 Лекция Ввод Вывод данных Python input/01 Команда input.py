a166abf2b6a9c8e31916926a8e0f384c7e912e0d
n.chudnovskiy <nikita.chudnovskiy@mail.ru>
2020-01-14 16:01:59 +0300

    add

--- 05 Лекция Ввод Вывод данных Python input/01 Команда input.py
+++ 05 Лекция Ввод Вывод данных Python input/01 Команда input.py
@@ -24,14 +24,4 @@ else:
     print('No')
 
 
-print(1, 2, 3, sep=',') # разделитель запятой
-print(4, 5, sep='???', end=', ') # end не переводит на следующую строку 6
-print(6, sep='123')  # Нужно как минимум 2 значения
 
-rub = 10
-kop = 99
-print('У меня есть %s рублей %s копеек' % (rub, kop))
-print('У меня есть {} рублей'.format(rub))
-
-rub,kop = map(int,input().split())
-print('Нам дали {} рублей {} копеек'.format(rub,kop))
