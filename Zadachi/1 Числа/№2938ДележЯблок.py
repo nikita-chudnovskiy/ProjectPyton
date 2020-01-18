bf1bdc2b26f12c27e7e166e26be72e1347024c15
n.chudnovskiy <nikita.chudnovskiy@mail.ru>
2020-01-18 11:11:11 +0300

    add

--- Zadachi/1 Числа/№2938ДележЯблок.py
+++
@@ -1,6 +0,0 @@
-# N школьников делят K яблок поровну, неделящийся остаток остается в корзинке. Сколько яблок достанется каждому школьнику?
-skolniki =int(input())
-yabloki = int(input())
-kolKagdomu = yabloki//skolniki
-kolVkorzine= yabloki%skolniki
-print('Яблок каждому',kolKagdomu,'яблок в корзине',kolVkorzine)
