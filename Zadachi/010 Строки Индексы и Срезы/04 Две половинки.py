35fdbd7a586dd7c7833d7ca7fca7f40a923ab3ba
n.chudnovskiy <nikita.chudnovskiy@mail.ru>
2020-01-22 19:07:02 +0300

    add

---
+++ Zadachi/010 Строки Индексы и Срезы/04 Две половинки.py
@@ -0,0 +1,6 @@
+# Две половинки
+x ='Hello'
+z = x[3:]
+y = x[:3]
+print(z)
+print(z+y)
---
+++ Zadachi/09 Строки и операции/Задачи строки.py
@@ -0,0 +1,8 @@
+s =input('Длинна строки qwerty')
+a ='a'
+print(len(s))
+
+print(s*2,'зудублировать')
+print(a*5)
+
+
