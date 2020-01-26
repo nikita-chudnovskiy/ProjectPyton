861630331c81124b6e1dc5e5cfe56c9e56172997
n.chudnovskiy <nikita.chudnovskiy@mail.ru>
2020-01-26 18:58:05 +0300

    add

---
+++ 015 Условный оператор if/01_If.py
@@ -0,0 +1,25 @@
+if 4 == 4 and 5 < 4:
+    print(1)
+    print(2)
+    print(3)
+else:
+    print(4)
+    print(5)
+print(6)
+
+print(' Любое не пустое значение в pythone преобразуется к истине')
+
+if 5:
+    print(1)
+    print(2)
+else:
+    print(3)
+print(4)
+
+
+if True:
+    print(99)
+    print(100)
+else:
+    print(101)
+print(4)
---
+++ 015 Условный оператор if/02_if_.py
@@ -0,0 +1,26 @@
+# Max из 2
+a = int(input())
+b = int(input())
+if a>b:
+    print(a)
+else:
+    print(b)
+
+
+
+# Вывести какое число
+a = int(input())
+if a%2 == 0:
+    print('chetnoe')
+else:
+    print('ne chetnoe')
+
+
+
+a = int(input())
+if a%2 == 0 and a % 3 == 0:  # Если будет делится на 2 но не на 3 то получим No
+    print('Yes')
+else:
+    print('No')
+
+
