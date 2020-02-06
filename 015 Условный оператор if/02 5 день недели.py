540d8475500c28d7e325cc1df817ccd9ab2d3bf6
n.chudnovskiy <nikita.chudnovskiy@mail.ru>
2020-02-06 15:22:51 +0300

    add

---
+++ 015 Условный оператор if/02 5 день недели.py
@@ -0,0 +1,13 @@
+# В питоне любое значение истино
+if 2:
+    print(1)
+if True:     # Также просто можем написать True
+    print(2)
+
+
+
+day = input('Какой пятый день недели')
+if day =='Friday' or day =='friday':
+    print('Пятница')
+else:
+    print('не верно')
