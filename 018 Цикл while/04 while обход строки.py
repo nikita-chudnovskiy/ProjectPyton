80a95d7157aadac73d38f3d346499bfc7a4b6cfe
n.chudnovskiy <nikita.chudnovskiy@mail.ru>
2020-02-22 19:21:54 +0300

    add

--- 018 Цикл while/04 while обход строки.py
+++ 018 Цикл while/04 while обход строки.py
@@ -1,4 +1,13 @@
-a = 'privet'
+a = '1ad23,236sdsRTY'
 while len(a)>0:
-    print(a[0])
-    a = a[1:]
+    bukva = a[0]
+    if bukva >='a'and bukva<='z':
+        print(bukva,'smol')
+    elif bukva >='A'and bukva <= 'Z':
+        print(bukva,'big')
+    elif bukva.isdigit():
+        print(bukva,'didgit')
+    else:
+        print('znak')
+
+    a = a[1:] # заменяем нашу строку на срез всех символов кроме первого
