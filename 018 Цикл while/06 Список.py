b4d3577825338bc971d5fb7dd60c78e18367bb75
n.chudnovskiy <nikita.chudnovskiy@mail.ru>
2020-03-02 16:44:04 +0300

    add

--- 018 Цикл while/06 Список.py
+++ 018 Цикл while/06 Список.py
@@ -1,11 +1,10 @@
 # Вывести все цифры в списке больше 100
 a = [102,19,2,101,3,5,4,2,6,103]
-
 while len(a)>0:
-    cifra = a[0]
+    cifra=a[0]
     if cifra>100:
-        print(cifra,end =' ')
-    a = a[1:]
+        print(a[0],end =' ')
+    a=a[1:]
 
 print()
 
