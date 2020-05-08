174eda706a57434b3a2c129b877eb385f4c77fbe
n.chudnovskiy <nikita.chudnovskiy@mail.ru>
2020-05-08 14:29:29 +0300

    add

---
+++ 018 Цикл while/021 0-5.py
@@ -0,0 +1,5 @@
+a =0
+i = 1
+while i<=5:
+    print(i,a)
+    i+=1
---
+++ 018 Цикл while/022 Сумма чисел от 0 до 100.py
@@ -0,0 +1,15 @@
+i =1
+summ=0
+while i <=5:
+    #print(i)
+    summ +=i
+    i+=1
+print(summ)
+
+
+a =[2,3,4]*3
+print(a)
+while 3 in a:
+   a.remove(3)
+
+print(a)
