b393a62494a613a3aa643c2ba771eaad25444f5e
n.chudnovskiy <nikita.chudnovskiy@mail.ru>
2020-05-09 14:13:00 +0300

    add

--- 018 Цикл while/04 while обход строки.py
+++ 018 Цикл while/04 while обход строки.py
@@ -1,31 +1,20 @@
-a ='privet'
+a ='privet' # Обход списка тоже самое
 while len(a)>0:
     print(a[0],a[1:])
     a=a[1:] # заменяем нашу строку на срез всех символов кроме первого
 
 # Обход строки
-a = '1aR#'
+a = '1abcR#$'
 while len(a)>0:
     bukva = a[0]
     if bukva >='a'and bukva<='z':
-        print(bukva,'smol',end=' ')
+        print(bukva,'smol')
     elif bukva >='A'and bukva <= 'Z':
-        print(bukva,'big',end=' ')
+        print(bukva,'big')
     elif bukva.isdigit():
-        print(bukva,'didgit',end=' ')
+        print(bukva,'didgit')
     else:
         print(bukva,'znak')
     a = a[1:] # заменяем нашу строку на срез всех символов кроме первого
 
-# Обход списка
-a = ['Привет', 99, 88, 77, 66, 55]
-while len(a) > 0:
-    print(a[0],end=' ')
-    a = a[1:] # заменяем нашу строку на срез всех символов кроме первого
-print()
-# 2 Вариант
-a = [1,2,3,'Привет']
-i = 0
-while i<len(a):
-    print(a[i],end=' ')
-    i=i+1
+
