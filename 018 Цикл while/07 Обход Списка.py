903011261c7ed4ee28464e6f119cbf4ad71c2cfe
n.chudnovskiy <nikita.chudnovskiy@mail.ru>
2020-05-13 15:33:00 +0300

    add

--- 018 Цикл while/07 Обход Списка.py
+++ 018 Цикл while/07 Обход Списка.py
@@ -1,4 +1,12 @@
 a =[1,2,3]
 while len(a)>0:
      print(a[0])
+
+     if a[0] >2:
+          print('больше')
+     elif a[0]==2:
+          print('равно')
+     else:
+          print('меньше ')
      a=a[1:]
+
