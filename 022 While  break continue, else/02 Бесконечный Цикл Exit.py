5be4e4f33788c580ec1e152100c47cfc6bf347d6
n.chudnovskiy <nikita.chudnovskiy@mail.ru>
2020-06-06 14:17:37 +0300

    add

--- 022 While  break continue, else/02 Бесконечный Цикл Exit.py
+++ 022 While  break continue, else/02 Бесконечный Цикл Exit.py
@@ -1,9 +1,11 @@
+count =0
 while True:
     a =input('Введите строку ')
+    count+=1
     if a =='exit':     # Бесконечно вводим строку пока не введем exit и выйдем из цикла
         break
-    print(a,'Длинна строки',len(a))
-
+    print('Длинна строки',len(a),'Вы ввели ',a)
+print('Попытка',count)
 
 
 
