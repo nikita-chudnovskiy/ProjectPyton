e9465e33940b8e075993d98d2b96a3066edbd2bd
n.chudnovskiy <nikita.chudnovskiy@mail.ru>
2020-07-21 13:27:57 +0300

    add

---
+++ Zadachi/025 for/07 Вывести слова где есть введенная буква список слов.py
@@ -0,0 +1,31 @@
+#Sample Input 1:
+
+#a
+#Mary had a little lamb.
+
+#Sample Output 1:
+#Mary
+#had
+#a
+#lamb.
+
+#a
+letter = input()
+text = 'Mary had a little lamb.'
+k=[]
+for i in text.split():
+    if letter in i.lower():
+        print(i)
+
+
+#e
+
+letter = input()
+text = 'Actions speak louder than words'
+k=[]
+for i in text.split():
+    if letter in i.lower():
+        print(i)
+
+
+
