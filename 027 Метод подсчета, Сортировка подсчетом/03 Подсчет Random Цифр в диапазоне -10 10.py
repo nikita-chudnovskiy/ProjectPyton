4137938fff720a419350d22466c978708f558241
n.chudnovskiy <nikita.chudnovskiy@mail.ru>
2020-08-13 14:48:33 +0300

    add

--- 027 Метод подсчета, Сортировка подсчетом/03 Подсчет Random Цифр в диапазоне -10 10.py
+++ 027 Метод подсчета, Сортировка подсчетом/03 Подсчет Random Цифр в диапазоне -10 10.py
@@ -5,7 +5,7 @@ for i in range(10):
 count = [0]*11 # Тк нужен 0 то -10 10 20 и еще 0 ТО пишем 21 Урок 27
 
 for i in a:
-    count[i+5]+=1
+    count[i+5]+=1 # смещение
 print(a)
 for i in range(11):
     if count[i]>0:
