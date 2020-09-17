386f6280ceca42a77b83e140e3d0ecb458f3d3d1
n.chudnovskiy <nikita.chudnovskiy@mail.ru>
2020-09-17 09:35:08 +0300

    add

--- 027 Метод подсчета, Сортировка подсчетом/03 Подсчет Random Цифр в диапазоне -10 10.py
+++ 027 Метод подсчета, Сортировка подсчетом/03 Подсчет Random Цифр в диапазоне -10 10.py
@@ -1,14 +1,14 @@
 import random
 a =[]
-for i in range(10):
-    a.append(random.randint(-5,5))
-count = [0]*11 # Тк нужен 0 то -10 10 20 и еще 0 ТО пишем 21 Урок 27
+for i in range(20):
+    a.append(random.randint(-10,10))
+count = [0]*21 # Тк нужен 0 то -10 10 20 и еще 0 ТО пишем 21 Урок 27
 
 for i in a:
-    count[i+5]+=1 # смещение
+    count[i+10]+=1 # смещение
 print(a)
-for i in range(11):
+for i in range(21):
     if count[i]>0:
-        print(i-5,count[i])
+        print(i-10,count[i])
 
         # При больших диапазонах не целесообразно будет съедать очень много памяти
