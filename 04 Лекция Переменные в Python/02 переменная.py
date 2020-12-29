c253dd6384e9e15666ac23213cd4c495dae9ebb4
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2020-12-29 18:55:04 +0300

    add

---
+++ 04 Лекция Переменные в Python/02 переменная.py
@@ -0,0 +1,16 @@
+age = 7
+
+name = 'Андрей'
+
+print(name,age,sep = " - ")
+
+print('Name :',name)
+print("Age :", age )
+
+name ='Анна' # Изменили значения переменных
+age = 38
+print(name,age)
+
+age = age+1 # изменили значение 39
+age+=1
+print(name,age)
