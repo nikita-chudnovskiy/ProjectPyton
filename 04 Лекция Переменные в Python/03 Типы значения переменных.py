d5efe2b354c10d68c1ca586ac02a679d1d9fcaf0
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2021-01-11 14:52:59 +0300

    add

---
+++ 04 Лекция Переменные в Python/03 Типы значения переменных.py
@@ -0,0 +1,13 @@
+
+# Типы переменных
+a = 10
+b =2.0
+c = 'hi'
+boolean = 0
+print(type(a)) # Тип int, integer
+print(type(b))
+print(type(c))
+print(type(a>b))
+
+b = int(b)
+print('изменили тип float',type(b))
