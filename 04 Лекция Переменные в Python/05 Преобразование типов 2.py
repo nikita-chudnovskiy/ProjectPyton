6b2e670d740adc8119b54f8cfd22ee11a91656cb
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2021-01-11 16:02:21 +0300

    add

---
+++ 04 Лекция Переменные в Python/05 Преобразование типов 2.py
@@ -0,0 +1,9 @@
+# Рейтинг 1: 4.5
+# Рейтинг 2: 3.2
+# Средний ave_rayting
+
+reiting1 = float(input('Рейтинг 1:'))
+reiting2 = float(input('Рейтинг 2:'))
+
+ave_rayting = (reiting1+reiting2)/2
+print(ave_rayting)
