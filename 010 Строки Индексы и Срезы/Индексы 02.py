a5ac4d3db31b5473a533fa21bd6c9e80793923cc
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2021-01-16 17:06:49 +0300

    add

--- 010 Строки Индексы и Срезы/Индексы 02.py
+++ 010 Строки Индексы и Срезы/Индексы 02.py
@@ -1,16 +1,18 @@
 password = 'qwerty'
 
-print(password[0:])    # qwerty
-print(password[0:3])   # qwe      0,1,2  до третьего индекса
-print(password[:])     # qwerty
-print(password[0:5:2]) # qet  шаг через 1
+print(password[0:])    # qwerty  начни с 0 и до конца
+print(password[0:3])   # qwe 0,1,2  до третьего индекса
+print(password[:])     # qwerty вся строка
+print(password[0:5:2]) # qet шаг через 1
 print(password[-1])    # y  последний
 print(password[:-1])   # до предпоследнего
 print(password[2:-1])
 
-
-
-
+#  ----------------------
+#  | H | e | l | p | A |
+#  ----------------------
+#  0   1   2   3   4   5
+# -5  -4  -3  -2  -1
 
 password=password[-1]+'nnn'+password
 print(password)
