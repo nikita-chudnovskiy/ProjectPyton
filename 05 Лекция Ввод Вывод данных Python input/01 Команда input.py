46c343d609a102a8f2f93e5b364ea62025acef98
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2021-01-20 16:10:13 +0300

    add

--- 05 Лекция Ввод Вывод данных Python input/01 Команда input.py
+++ 05 Лекция Ввод Вывод данных Python input/01 Команда input.py
@@ -1,12 +1,10 @@
 # 13.01.2020
 
-a = input()                                        # str Ввести строку Строку
+a = input('введите симвлы: ')  # str Ввести строку Строку
 
-a,b,c= map(int,input().split())                    #  ввести 3 числа
+a, b, c = map(int, input('Ввести 3 числа через пробел ').split())  # ввести 3 числа
 
-print(sum(map(int, input('Сумма всех').split())))  # вывести сумму
-
-
+print(sum(map(int, input('Введите 3 числа через пробел Сумма всех: ').split())))  # вывести сумму
 
 # Проверяем есть ли в веденной строке имя Nikita
 name = (input('Проверка имени '))
@@ -14,6 +12,3 @@ if 'Nikita' in name:
     print('Success!')
 else:
     print('No')
-
-
-
