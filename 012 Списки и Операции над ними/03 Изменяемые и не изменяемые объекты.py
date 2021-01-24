c5969461c78f6989b62f0adc9e60a2b783812adb
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2021-01-24 18:15:20 +0300

    add

--- 012 Списки и Операции над ними/03 Изменяемые и не изменяемые объекты.py
+++ 012 Списки и Операции над ними/03 Изменяемые и не изменяемые объекты.py
@@ -3,7 +3,13 @@ my_list =[1,2,3,4]
 
 second_list =my_list
 my_list.append(100)
-print(second_list)
+print(id(second_list),second_list,id(my_list),my_list) # id так не меняется , а если скопируем то поменяется
+
+new = my_list.copy()  # а если скопируем то поменяется
+new.append(999)
+print(id(new),new)
+
+
 
 # Не изменяемые объекты это числа и строки (меняется id если мы изменяем, создается новый объект)
 valve=42
