4ba64a5fead5668deadce3ade56f663e2aec4fac
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2021-01-28 15:58:30 +0300

    add

--- 031 Множества Тип данных set/01.py
+++ 031 Множества Тип данных set/01.py
@@ -1,8 +1,9 @@
-list1 =[1,3,3,3,3,3,4,9]
-list2=[2,2,2,2,4,4,4]
+list1 = [1, 3, 3, 3, 3, 3, 4, 9]
+list2 = [2, 2, 2, 2, 4, 4, 4]
 
-list3 = (list1&list2)
+list3 = (set(list1) | set(list2))
+print(list3)
 
 
-print(set(list3))
-
+a =['Июнь','Июль','Июнь']
+print(set(a))
