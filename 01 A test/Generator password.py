ad00dd358cb15ea0106c9c938ca235712dba8fbf
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2021-03-30 14:07:01 +0300

    add

---
+++ 01 A test/Generator password.py
@@ -0,0 +1,7 @@
+from  random import randint
+
+for i in range(5):
+    a = randint(1,10)
+    b = randint(ord('A'),ord('z'))
+    c= randint(ord('#'),ord('*'))
+    print(a,chr(b),chr(c), end=' ')
--- 01 A test/test.py
+++
@@ -1,2 +0,0 @@
-a = iter('nikita')
-print(next(a))
