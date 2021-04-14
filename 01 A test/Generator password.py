f36549ae2434d4e53ccc710ab22ccd0635395798
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2021-04-14 10:08:26 +0300

    add

--- 01 A test/Generator password.py
+++ 01 A test/Generator password.py
@@ -4,5 +4,5 @@ for i in range(5):
     a = randint(1,10)
     b = randint(ord('A'),ord('z'))
     c= randint(ord('#'),ord('*'))
-    print(a,chr(b),chr(c), end=' ')
-f =int(input())
+    print(a,chr(b),chr(c),end='',sep='')
+zzz = int(input())
