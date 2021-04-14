74f8329ebf48483a2c1a96295f80a66a744e076e
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2021-04-14 14:31:19 +0300

    add

--- Zadachi/032 Словари/032 Словари.py
+++ Zadachi/032 Словари/032 Словари.py
@@ -3,7 +3,7 @@ person ={}
 s =' Ivanov Ivan Samara 5 4 3'
 s =s.split()
 
-
+# Создали ключи и присвоили им значения строки 0 1 2 , далее три значения закинули в 4 ключ Оценки
 person['lastName'] =s[0]
 person['firstName'] =s[1]
 person['City'] =s[2]
