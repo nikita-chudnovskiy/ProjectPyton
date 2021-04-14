dfc18bd3808adabf73f2b4bfae2f7f7e18493443
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2021-04-14 14:20:59 +0300

    add

--- Zadachi/032 Словари/032 Словари.py
+++ Zadachi/032 Словари/032 Словари.py
@@ -1,8 +1,15 @@
-a = {
+person ={}
 
-    'moskva': 495,
-    'Piter': 812,
-     3: 1
-}
-print(a['moskva'])
-print(a)
+s =' Ivanov Ivan Samara 5 4 3'
+s =s.split()
+
+
+person['lastName'] =s[0]
+person['firstName'] =s[1]
+person['City'] =s[2]
+person['Ocenki']=[]   # создали пустой список
+
+for i in s[3:]: # в 4 элемент оценки
+    person['Ocenki'].append(int(i))
+print(s)
+print(person)
