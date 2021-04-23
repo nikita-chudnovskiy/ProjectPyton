d8d13d4b6b28230b3ffe01983a445e8cd3efd795
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2021-04-23 17:55:21 +0300

    add

--- 01 A test/01 подсчет букв.py
+++ 01 A test/01 подсчет букв.py
@@ -1,2 +1,10 @@
-stroka =input()
-print(stroka.count('a'))
+b =input()
+
+slovar={}
+
+for i in b:
+    if i in slovar:
+        slovar[i]+=1
+    else:
+        slovar[i]=1
+print(slovar)
--- 033 4 Ситуации где полезно использовать словарь/03 Подсчет кол букв в словах.py
+++ 033 4 Ситуации где полезно использовать словарь/03 Подсчет кол букв в словах.py
@@ -1,9 +1,10 @@
 example = input()
 
 count = {}
-
+g=0
 for i in example.split():
     count[i] = len(i)
-
+    g =g+1
+print(count,g)
 
 
