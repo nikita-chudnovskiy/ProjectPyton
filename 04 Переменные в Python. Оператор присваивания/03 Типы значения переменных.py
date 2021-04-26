09a20ccf4a91744408916c4a81e877472d7ad6fc
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2021-04-26 14:16:58 +0300

    add

--- 04 Переменные в Python. Оператор присваивания/03 Типы значения переменных.py
+++ 04 Переменные в Python. Оператор присваивания/03 Типы значения переменных.py
@@ -1,15 +1,10 @@
-
 # Типы переменных
 
-name = 'Anna'           # str
-age = 10                # int
-height =1.67            # float
-is_maile = True         # bool
+name = 'Anna'  # str
+age = 10  # int
+height = 1.67  # float
+is_maile = True  # bool
 
-
-print(type(name),type(age),type(height),type(is_maile),sep =' # ') # sep разделитель
-print(type(name),type(age),type(height),type(is_maile),end =',')  # чтобы Анна не перенес на другую строчку
+print(type(name), type(age), type(height), type(is_maile), sep=' # ')  # sep разделитель
+print(type(name), type(age), type(height), type(is_maile), end=',')  # чтобы Анна не перенес на другую строчку
 print(name)
-
-
-
--- 04 Переменные в Python. Оператор присваивания/06 Домашнее задание.py
+++ 04 Переменные в Python. Оператор присваивания/06 Домашнее задание.py
@@ -1,39 +1,38 @@
 import math
+
 # 9 во 2 степени + 6 в 4 степени
 
-print((9**2)+(6**4))
-print(pow(6,4))
+print((9 ** 2) + (6 ** 4))
+print(pow(6, 4))
 
 # Корень 8 - корень 12
-a =math.sqrt(8)
+a = math.sqrt(8)
 b = math.sqrt(12)
-print(a-b)
-print(math.sin(45)+math.cos(45))
+print(a - b)
+print(math.sin(45) + math.cos(45))
 
 # Размер туфель
 t = int(input('Введите размер скальных туфель'))
-print('Размер скальных туфель',t-2)
+print('Размер скальных туфель', t - 2)
 
 # Сумма баллов за 3 теста
-test1=int(input('введите балл '))
-test2=int(input('введите балл '))
-test3=int(input('введите балл '))
-itog =test1+test2+test3
-print('итоговый бал',itog)
-
+test1 = int(input('введите балл '))
+test2 = int(input('введите балл '))
+test3 = int(input('введите балл '))
+itog = test1 + test2 + test3
+print('итоговый бал', itog)
 
 # Введите зарплата
 salary = int(input('Введите зарплату'))
 
-salary_th=salary//1000
+salary_th = salary // 1000
 
-print('Ваша зарплата %s тысяч рублей.' %(salary_th))
-
+print('Ваша зарплата %s тысяч рублей.' % (salary_th))
 
 # Сумма перевода с комиссией
 sum_perevoda = float(input('Введите сумму перевода с комиссией 5 %'))
 
-sum_perevoda += sum_perevoda/100*5
+sum_perevoda += sum_perevoda / 100 * 5
 
 print(sum_perevoda)
 
@@ -42,9 +41,9 @@ per1 = (input())
 per2 = (input())
 per3 = (input())
 per4 = (input())
-itog = per1+per2+per3+per4
+itog = per1 + per2 + per3 + per4
 print(itog)
 
 # След четное
 s = int(input())
-print(s+2-(s%2))
+print(s + 2 - (s % 2))
