8b931bfdab896721bf30f02123abcbeeef962c7a
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2021-04-26 14:16:25 +0300

    add

--- 03 Лекция Знакомство с Обьектами числа и Опрации над ними/04 from math import sqrt.py
+++ 03 Лекция Знакомство с Обьектами числа и Опрации над ними/04 from math import sqrt.py
@@ -3,7 +3,7 @@ import math
 
 print(sqrt(25))
 print(math.factorial(4))  # 1*2*3*4 Перемноженные все числа
-print(1*2*3*4)
+print(1 * 2 * 3 * 4)
 
-print(round(2.63,1)) # округдение до 1 знака
+print(round(2.63, 1))  # округдение до 1 знака
 #
