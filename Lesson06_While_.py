a901967863aaec4b5fa8ca4b71a5dc2445d35a68
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2021-04-26 11:44:49 +0300

    add

--- Lesson06_While_.py
+++
@@ -1,8 +0,0 @@
-i = 0
-while i <= 10:
-    i+=1
-    print(i +2)
-    if i > 0:
-        print('   ',i+1)
-
-
--- Work.py
+++
@@ -1,7 +0,0 @@
-helloFile =  open('C:\\example\\Work.txt', 'r')
-n=helloFile.read()
-print(n)
-
-
-
-# https://tokmakov.msk.ru/blog/item/45
