a1af8302ab49c5d5ccaa461567e1ff948406a87b
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2021-04-26 10:33:29 +0300

    add

--- 032 Словари (dict) Python. Операции и методы словаря/08 Словарь.py
+++ 032 Словари (dict) Python. Операции и методы словаря/08 Словарь.py
@@ -1,13 +1,8 @@
-my_box ={'слон':'элефант',2:'2'}
-print(my_box)
-
-my_box[3]='three'
-print(my_box)
-
-# set Нам просто добавит если нет Мурзика
-print(my_box.get('kot','Мурзик'))
-print(my_box)
-
-# setdefault добавит в словарь ключ кот и Мурзик
-my_box.setdefault('Кот','Мурзик')
-print(my_box.items())
+worlds ={}
+while True:
+    s =input()
+    if s in worlds:
+        print('Слово',s,'переводится как',worlds[s])
+    else:
+        print('вв перевод слова',s)
+        worlds[s]=input()
