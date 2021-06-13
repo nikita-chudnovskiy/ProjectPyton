4d02bed4b24a547ad47ed4a1b2aed8da5dcb5efa
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2021-06-13 14:33:33 +0300

    add

---
+++ 01 A test/for i peremennaya.py
@@ -0,0 +1,12 @@
+spisok = ['mersedes','audi']
+
+for i in range(len(spisok)):
+    car =spisok[i]
+    print(i,car)
+
+print()
+
+spisok2 = ['sobaka','kot']
+for i ,animals in enumerate(spisok2):
+    print(i,animals)
+
--- 01 A test/if.py
+++
@@ -1,5 +0,0 @@
-a =1
-if a ==1:
-    pass
-else:
-    print('другое')
