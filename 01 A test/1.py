26176a804ef311eaa8e69b90e833161b38aebe18
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2022-02-03 09:44:35 +0300

    add

--- 01 A test/1.py
+++ 01 A test/1.py
@@ -1,11 +1,2 @@
-import math
-while True:
-    s = input('Введите что-нибудь : ')
-    if s == 'выход':
-        break
-    if len(s) < 3:
-        print('Слишком мало')
-        continue
-    print('Введённая строка достаточной длины')
-
-2**3
+*a,b,c = 1,2,3,4
+print(a,b,c)
--- 01 A test/123.py
+++
@@ -1,7 +0,0 @@
-a = ord('А')
-
-for i in range(33):
-    if a <=ord('Я'):
-        print(chr(a),end=' ')
-        a+=1
-
