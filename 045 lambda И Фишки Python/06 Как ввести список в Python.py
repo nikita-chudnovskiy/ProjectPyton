dc285383b69c55533cb44f30a3cf42f1aadf79d2
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2022-02-17 09:25:25 +0300

    add

---
+++ 045 lambda И Фишки Python/06 Как ввести список в Python.py
@@ -0,0 +1,14 @@
+a =input().split() # Выведет список со строками
+print(a)
+
+
+ # Ввесьти список чисел
+a = list(map(int,input().split())) # map преобразовывает в целое число
+print(a)
+
+
+
+
+# Присвоить больше одной переменной и вывести значения int
+rub,kop = map(int,input().split())
+print(f"{rub},{kop}")
