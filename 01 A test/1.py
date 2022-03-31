86d2fa241c20b75d43128c4a964d7161d81db7db
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2022-03-31 14:49:24 +0300

    add

--- 01 A test/1.py
+++ 01 A test/1.py
@@ -1,12 +1,24 @@
-import random
 
+a =[('Вася',18),
+    ('Федя',19),
+    ('Васек',20)
+    ]
+
+
+
+
+b = [i for i in a if i[0].startswith('В') and i[1]<19 ]
+print(b)
+
+print()
 a ={
-    'Lупкин':{'age':100},
-    'Iора':{'age':199},
-    'KВас':{'age':200},
-    'EВас':{'age':200}
+    'Васek':{'age':19,'auto':'BMW'},
+    'Вася':{'age':17,'auto':'BMW'},
+    'ФЕДЯ':{'age':20,'auto':'Marsedes'},
+    'Коля':{'age':21,'auto':'Shkoda'},
 
-}
+    }
 
-b =[elem[0] for  elem in a  ]
+
+b = [(i,a[i]['auto']) for i in a if a[i]['age']>18 and a[i]['auto']=='BMW' ]
 print(b)
---
+++ 01 A test/zzzРасписание.py
@@ -0,0 +1,39 @@
+# Множественнй Выбор если условие не верно то пропускает и идет к следующему
+
+n1 = [
+    ['Математика', 'Русский', 'Чтение', 'Изо', 'Физкультура'],
+    ['Русский', 'Математика', 'Английский', 'Окр Мир', 'Шахматы'],
+    ['Физкультура', 'Математика', 'Русский', 'Чтение', ],
+    ['Английский', 'Русский', 'Математика', 'Чтение', 'Труд'],
+    ['Математика', 'Чтение', 'Окр Мир', 'Музыка']
+]
+while True:
+    n = int(input('Расписание Нажмите от 1  до 5 и Enter, 10 Выход '))
+
+    print()
+    if n == 1:
+
+        print('Понедельник:')
+        for i, value in enumerate(n1[0], 1):
+            print(i, value)
+
+    elif n == 2:
+        print('Вторник:')
+        for i, value in enumerate(n1[1], 1):
+            print(i, value)
+    elif n == 3:
+        print('Среда:')
+        for i, value in enumerate(n1[2], 1):
+            print(i, value)
+
+    elif n == 4:
+        print('Четверг:')
+        for i, value in enumerate(n1[3], 1):
+            print(i, value)
+    elif n == 5:
+        print('Пятница:')
+        for i, value in enumerate(n1[4], 1):
+            print(i, value)
+    elif n == 10:
+        print('End')
+        break
