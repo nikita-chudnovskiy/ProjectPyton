9ea0c9d97f68ab67ddd4717fdd551231b289fc2c
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2022-07-09 11:27:55 +0300

    add

--- 01 A test/1.py
+++ 01 A test/1.py
@@ -1,18 +1,6 @@
-# def ZnacenieBolshe10(x):
-#     return x>10
-#
-#
-# a = [2,11,12,14]
-#
-# a=list(filter(ZnacenieBolshe10,a))
-# print(a)
-#
-b =[]
-c =[2,3,5,11,12,44,47]
-for i in range(len(c)):
-    if c[i]<10  or c[i]<45:
-        b.append(c[i])
-print(b)
+a ='asdasd 45'
 
-a =[i for i in c if i <=10]
-print(a)
+
+a=filter(str.isdigit,a)
+print(list(a))
+
---
+++ 053 Метод split  и join/01 Метод split и join.py
@@ -0,0 +1,6 @@
+a='dadaddad  45 fsfsdfs 32'
+a1=('dadaddad',  45, 'fsfsdfs', 32)
+
+a1=a1.split()
+
+print(a1)
