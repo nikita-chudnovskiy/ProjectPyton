f67ef40a6287b3962c4e9b448c428351fa7755b6
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2022-07-13 13:28:42 +0300

    add

---
+++ 054.2 Экранированные символы/01 Экранированные символы.py
@@ -0,0 +1,22 @@
+s ="""hello
+world"""
+print(s)
+
+a ='abc\ndeg'
+print(a)
+print(len(a))# Служебный символ \n считает за один
+
+a ='fff\nggg\tjjj\nooo' # если поставить r перед кавычками  'fff\nggg\tjjj\nooo' никакого форматирования не произойдет
+print(a)
+
+
+
+
+a ='f\ng\tj\no'
+print(a)
+print(len(a))
+a =r'f\n\tj\no'
+print(a)
+print(len(a))
+
+print('C:\ \default')
