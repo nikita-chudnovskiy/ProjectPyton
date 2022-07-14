60be8fa92ca31dd07013d29541143b17065b043b
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2022-07-14 13:44:46 +0300

    add

--- 055 Установка сторонних Модулей/01 Установка сторонних Модулей.py
+++ 055 Установка сторонних Модулей/01 Установка сторонних Модулей.py
@@ -1,4 +1,22 @@
 # Как можно в pythone установить сторонние и внешние модули
 # Внешний модуль представляет такую же программу которая уже написана до Вас другим разработчиком
 
-import
+# pypi.org 		# сайт с модулями
+#
+#
+# python.exe -m pip install --upgrade pip
+#
+#
+# pip list   			# посмотреть пакеты
+# pip freeze 			# посмотреть пакеты
+#
+# pip freeze > test.txt   	# Выгрузить список моих пакетов
+# pip freeze > recuirements.txt   # Выгрузить список моих пакетов
+#
+#
+#
+# pip install flask 		# Установить отдельный пакет
+#
+# pip install -r recuirements.txt # Установить сразу список нужных пакетов из файла
+#
+# pip uninstall -r recuirements.txt удалить пакеты
