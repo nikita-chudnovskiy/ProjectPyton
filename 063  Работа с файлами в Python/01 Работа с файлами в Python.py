aa4df82c2b67149f0d6e140d67d4fa93ce9c3863
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2022-08-15 11:02:42 +0300

    add

--- 063  Работа с файлами в Python/01 Работа с файлами в Python.py
+++ 063  Работа с файлами в Python/01 Работа с файлами в Python.py
@@ -1,4 +1,28 @@
 # Работа с файлами
 
 file = open('111.txt',encoding ='utf-8') # Указать кодировку
+file2 =open(r'C:\1.txt',encoding ='utf-8') # r будет убирать все служебные символы
 print(file.read())
+#print(file2.read())
+
+# Как корретка считывает последовательно уже не выведет
+# если нужно откатится то file2.seek(0)
+#file2.seek(0)
+#print(file2.readline())
+#print(file2.readline())
+
+for i in file2:
+    print(i,end='')
+
+file2.seek(0)
+print()
+print(file2.read())
+
+file.seek(0)
+
+for row in file:
+    for letter in row:
+        print(letter)
+
+
+
