f0b93d11b145558c60a21676fa94199a7b9380b9
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2022-08-16 10:10:06 +0300

    add

--- 01 A test/1.py
+++ 01 A test/1.py
@@ -1,4 +1,2 @@
 from random import randint
 
-a =['a','b',1]
-print(all(a))
--- 063  Работа с файлами в Python/03.py
+++ 063  Работа с файлами в Python/03.py
@@ -1,9 +1,25 @@
-file = open(r'TCS.txt',encoding='utf-8')
+file = open(r'file.txt',encoding='utf-8')
 
-s =file.readlines() # Сделать списком
+s=file.readlines()
 print(s)
 
+khutor =[]
+
 for i in range(len(s)):
-    print(i,s[i],end='')
+       print(s[i])
+       if s[i]=='tcs1111\n':
+              khutor.append(s[i])
+print(khutor)
+
+
+
+
+
+
+
+
+
+
+
 
 
