e74d81a0d6beab88f92878ebb65d92d175a8182f
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2022-09-15 13:19:23 +0300

    add

--- 01 A test/Generator password.py
+++ 01 A test/Generator password.py
@@ -1,22 +1,21 @@
 from random import randint
 
-for i in range(5):
-    a = randint(1, 10)
-    b = randint(ord('A'), ord('z'))
-    c = randint(ord('#'), ord('*'))
-    print(a, chr(b), chr(c), end='', sep='')
-zzz = int(input())
-
+# for i in range(5):
+#     a = randint(1, 10)
+#     b = randint(ord('A'), ord('z'))
+#     c = randint(ord('#'), ord('*'))
+#     print(a, chr(b), chr(c), end='', sep='')
+# zzz = int(input())
+count =0
 while True:
-    print('  Нажмите от 1 до 9 Если Вам нужно выйти нажмите 10 ')
-    n = 5
-    zzz = int(input())
-    if zzz < 10:
 
-        for i in range(n):
-            a = randint(1, 10)
-            b = randint(ord('A'), ord('z'))
-            c = randint(ord('#'), ord('*'))
-            print(a, chr(b), chr(c), end='', sep='')
-    elif zzz == 10:
+    count +=1
+    n = int(input('Задайте количество символов'))
+
+    for i in range(n):
+         a = randint(1, 10)
+         b = randint(ord('A'), ord('z'))
+         c = randint(ord('#'), ord('*'))
+         print(a, chr(b), chr(c), end='', sep='')
+    if count ==5:
         break
