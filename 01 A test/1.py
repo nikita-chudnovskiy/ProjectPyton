813419a880b1676a655ce267c3bf9a11befd4030
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2022-09-21 12:02:03 +0300

    add

--- 01 A test/1.py
+++ 01 A test/1.py
@@ -1,4 +1,20 @@
-a =list(range(0,10))
-print(a)
-a =list(range(9,-1,-1))
-print(a)
+import os
+import socket
+import ipaddress
+from ipaddress import IPv4Address
+
+import getpass
+
+
+
+net=IPv4Address('192.168.1.0/24')
+print(net)
+# ip_address=socket.gethostbyname('DESKTOP-124NN70')
+# print(ip_address)
+
+# if IPv4Address('192.168.1.3') in net:
+#     print(ip_address)
+
+
+
+
