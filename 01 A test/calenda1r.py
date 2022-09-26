dc0f84327b54ae82f78206f454b768759a1939d6
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2022-09-26 13:01:40 +0300

    add

--- 01 A test/calenda1r.py
+++ 01 A test/calenda1r.py
@@ -1,4 +1,11 @@
 import calendar
 
+god =int(input('Введите год !'))
+print()
 c=calendar.TextCalendar()
-print(c.formatyear(2022))
+print(c.formatyear(god))
+
+file =open(r'C:\My Python\ProjectPyton\01 A test\calendar.txt','a+',encoding ='utf-8')
+print(file.read())
+file.write(c.formatyear(god))
+a=int(input())
