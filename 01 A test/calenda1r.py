d1dc9e1c2acaed0fb3cce66c1f49aa3bef6c9d0f
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2022-09-26 13:07:46 +0300

    add

--- 01 A test/calenda1r.py
+++ 01 A test/calenda1r.py
@@ -1,5 +1,5 @@
 import calendar
-
+# Как работать с файлами это использовать менеджеры контекстов
 god =int(input('Введите год !'))
 print()
 c=calendar.TextCalendar()
@@ -8,4 +8,5 @@ print(c.formatyear(god))
 file =open(r'C:\My Python\ProjectPyton\01 A test\calendar.txt','a+',encoding ='utf-8')
 print(file.read())
 file.write(c.formatyear(god))
+file.close()
 a=int(input())
