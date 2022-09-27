9d8cb5cdb9f53d65ee0e908ec033d5f4a746bcdd
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2022-09-27 11:48:38 +0300

    add

--- 01 A test/calenda1r.py
+++ 01 A test/calenda1r.py
@@ -6,7 +6,8 @@ c=calendar.TextCalendar()
 print(c.formatyear(god))
 
 file =open(r'C:\My Python\ProjectPyton\01 A test\calendar.txt','a+',encoding ='utf-8')
-print(file.read())
+#print(file.read())
+
 file.write(c.formatyear(god))
 file.close()
-a=int(input())
+a=input('для выхода нажмите любую клавишу')
