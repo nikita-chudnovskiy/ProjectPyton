5bbb955b1268afe8dd4b6044045a7f5b67fa33aa
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2022-09-27 12:37:18 +0300

    add

--- 01 A test/calenda1r.py
+++ 01 A test/calenda1r.py
@@ -4,8 +4,8 @@ god =int(input('Введите год !'))
 print()
 c=calendar.TextCalendar()
 print(c.formatyear(god))
-
-file =open(r'C:\My Python\ProjectPyton\01 A test\calendar.txt','a+',encoding ='utf-8')
+                                                            # w перезапись , a++ добавить
+file =open(r'C:\My Python\ProjectPyton\01 A test\calendar.txt','w',encoding ='utf-8')
 #print(file.read())
 
 file.write(c.formatyear(god))
---
+++ 063  Работа с файлами в Python/Ввести слова и записать в файл.py
@@ -0,0 +1,10 @@
+
+#Ввести слова и добавить
+
+slova=input('Введите слова')          # a+  это ключ означает что можем добавлять
+file =open(r'C:\My Python\ProjectPyton\063  Работа с файлами в Python\1.txt','a+',encoding ='utf-8')
+print(file.read())
+#file.write(c.formatyear(god))
+file.write(f'{slova}\n')
+file.close()
+
