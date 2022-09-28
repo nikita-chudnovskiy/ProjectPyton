d5e3ece4b235bb5491439457bcec085ac161aede
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2022-09-28 12:46:28 +0300

    add

---
+++ 070 Импорт собственных модулей/dir/my_modul.py
@@ -0,0 +1,10 @@
+
+import sys
+from pprint import pprint
+from
+
+pprint(sys.path)
+
+
+
+
--- 070 Импорт собственных модулей/import_library.py
+++ 070 Импорт собственных модулей/import_library.py
@@ -1,7 +1,5 @@
 from math import *  # импортируем из модуля match все имена
 
-#x,y =map(int,input().split())
-
 
 def summa_vseh(*args):
     return sum(args)
