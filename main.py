93cf0f3aa8b26e3cdafe5252253c648e7f23e566
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2022-10-06 12:59:17 +0300

    add

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-import package1.file1
-from package1 import file2 # Теперь не нужно писать  package2.file2.a
-from package1.file2 import c  # Обращаемся уже напрямую
+#import package1
+#from package1 import file2 # Теперь не нужно писать  package2.file2.a
+#from package1.file2 import c  # Обращаемся уже напрямую
 
-print(package1.file1.d)
-print(file2.a)
-print(c)
+#print(package1.file1.d)
+#print(file2.a)
+
+#package1.a # уже нет доступа к переменной d !!!
+#package1.a # __all__ мы видем что доступ только к a
+
+from package1.file1 import  d
+
 
