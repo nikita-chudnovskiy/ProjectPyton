e4acfe1374dd75284cb00818dab83ed8abfb8257
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2022-10-09 17:40:32 +0300

    add

--- 074 Чтение файлов из Exel/libra.py
+++ 074 Чтение файлов из Exel/libra.py
@@ -1,7 +1,7 @@
 import openpyxl
 
 book = openpyxl.open("2.xlsx",read_only=True)
-#a = book.active
+a = book.active
 #print(a[2][0].value)
 
 # for i in range(1,a.max_row+1):
@@ -16,9 +16,9 @@ book = openpyxl.open("2.xlsx",read_only=True)
 #     print(name.value,year.value,rating.value)
 
 
-# for row in a.iter_rows(min_row=1,max_row=20,min_col=1,max_col=5): # Если не передавать значение то выведет весь
-#     for cell in row:
-#         print(cell.value,end =' ')
-#     print()
-sheets_2 = book.worksheets[2] # 0,1,2 листы !!!!начинаются с 0
-print(sheets_2["A1"].value)
+for row in a.iter_rows(): # Если не передавать значение то выведет весь  (min_row=1,max_row=20,min_col=1,max_col=5)
+    for cell in row:
+        print(cell.value,end =' ')
+    print()
+# sheets_2 = book.worksheets[2] # 0,1,2 листы !!!!начинаются с 0
+# print(sheets_2["A1"].value)
