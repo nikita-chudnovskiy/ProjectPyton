62fceabfb47089a82af6eb3907bb6b58f2e95368
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2022-11-17 13:09:01 +0300

    add

--- 01 A test/02.py
+++ 01 A test/02.py
@@ -1,19 +1,8 @@
+import random
 
-
-with open(r"C:\Users\user\Desktop\Пушкин А.С..txt", 'w', encoding='utf-8') as file:
-  for i in range(5):
-    date=file.write('1\n')
-
-
-
-with open(r"C:\Users\user\Desktop\Пушкин А.С..txt", 'r', encoding='utf-8') as file:
-  date=file.read()
-print(date,end=' ')
-
-
-
-
-
+b = [random.randint(1,10)  for i in range(5) ]
+print(b)
+print(random.sample(b,3))
 
 
 
--- 047 Генераторы списков 2/05.py
+++ 047 Генераторы списков 2/05.py
@@ -4,6 +4,13 @@ name = ['Вася','Петя','Андрей']
 d = ['гулял','играл','плакал','бесился']
 
 z = [name,d]
+print(z)
 
 l =[random.choice(i)  for i in z]
 print(l)
+
+
+
+b = [random.randint(1,10)  for i in range(5) ]
+print(b)
+print(random.sample(b,3))
