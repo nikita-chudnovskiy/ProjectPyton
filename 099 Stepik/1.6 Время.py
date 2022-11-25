a6eb1d5ce72cff3fdbd50ca671ce5819a9580a7f
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2022-11-25 17:24:01 +0300

    add

---
+++ 099 Stepik/1.6 Время.py
@@ -0,0 +1,10 @@
+h1 = int(input('number'))
+m1 = int(input('number'))
+s1 = int(input('number'))
+h2 = int(input('number'))
+m2 = int(input('number'))
+s2 = int(input('number'))
+
+t1= (h1*3600)+(m1*60)+s1
+t2 = (h2*3600)+(m2*60)+s2
+print(t2-t1)
