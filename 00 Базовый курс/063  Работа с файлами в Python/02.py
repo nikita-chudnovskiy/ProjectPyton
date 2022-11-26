2c0a63fc9195a2b7825314aef07ec1dee3e5856d
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2022-11-26 13:25:29 +0300

    add

---
+++ 00 Базовый курс/063  Работа с файлами в Python/02.py
@@ -0,0 +1,8 @@
+file = open('Брат2.txt', 'a+', encoding='utf-8') # r -чтение w -запись  a - допишется в в конец, a+ чтение и запись
+s= file.readlines() # Поместить в список
+print(s)
+file.write('AAAAAAAA\n')
+file.write('AAAAAAAA\n')
+file.seek(0)
+
+
