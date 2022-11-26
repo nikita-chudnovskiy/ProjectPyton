f2a57fb13c343101dd00117d3062326da59b7bfb
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2022-11-26 13:56:56 +0300

    add

---
+++ 00 Базовый курс/035 Функции (def)/00 Функии.py
@@ -0,0 +1,9 @@
+def some_func(): # def determine ( c англ определить)
+    print('я функция')
+
+
+def square(x,y,z): # параметры функции
+    return x*y*z
+s =square(3,3,3) # ( то что в скобках это аргументы функции)
+
+print(f'площадь треугольника = {s}')
