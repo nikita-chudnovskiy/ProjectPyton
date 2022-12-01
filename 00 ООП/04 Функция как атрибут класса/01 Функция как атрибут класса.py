5fc680e01a5d1b524f2e2871ec31eabc7e66c4d2
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2022-12-01 12:32:24 +0300

    add

--- 00 ООП/04 Функция как атрибут класса/01 Функция как атрибут класса.py
+++ 00 ООП/04 Функция как атрибут класса/01 Функция как атрибут класса.py
@@ -3,6 +3,12 @@ class Car:
     engine = 1.8
 
     def drive():
-        print("Lets go")
+        print("Let's go")
 Car.drive()
+
+print(Car.__dict__)
+a=Car()
+print(a.__dict__)
+
+print(a.drive()) # Ругается тк в классе !!! в функции должно быть параметры функции
 
