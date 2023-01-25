0cba96acc9ce51d042d2bac267a14dca16f86393
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2023-01-25 13:05:35 +0300

    add

--- 00 Базовый курс/01 A test/test2.py
+++ 00 Базовый курс/01 A test/test2.py
@@ -1,150 +1,19 @@
-m28=[2]
-m31=[1,3,5,7,8,10,12]
-m30 =[4,6,9,11]
-t = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-m, num = map(int, input().split())
-if m in m28 and num <= 28 and m<=12 and num <=31:
+"""
+    Программа Python для фильтрации нечетных чисел
+    в списке, используя функцию filter()
+"""
 
-    if num ==28:
-        m = str(m)
+# список чисел
+numbers = [1, 2, 4, 5, 7, 8, 10, 11]
 
-        num0 = int(num) - 1
-        num0 = str(num0)
+# функция, которая проверяет числа
+def ne_chet(in_num):
+    if(in_num % 2) == 1:
+        return True
+    else:
+        return False
 
-        num = 1
-        num = str(num)
+# # Применение filter() для удаления нечетных чисел
+out_filter = list(filter(ne_chet,numbers))
 
-        m1 = int(m) + 1
-        m1 = str(m1)
-
-        print(m.rjust(2, '0') + f"." + num0.rjust(2, "0"), m1.rjust(2, '0') + f"." + num.rjust(2, '0'))
-    elif m in m28 and num <=27 and num >=2:
-        m = str(m)
-
-        num0 = int(num) - 1
-        num0 = str(num0)
-
-        num = num +1
-        num = str(num)
-
-        m1 = int(m)
-        m1 = str(m1)
-
-        print(m.rjust(2, '0') + f"." + num0.rjust(2, "0"), m1.rjust(2, '0') + f"." + num.rjust(2, '0'))
-
-    elif num == 1:
-        m = 1
-        m = str(m)
-
-        num0 = 31
-        num0 = str(num0)
-
-        num = num+1
-        num = str(num)
-
-        m1 = int(m)+1
-        m1 = str(m1)
-
-        print(m.rjust(2, '0') + f"." + num0.rjust(2, "0"), m1.rjust(2, '0') + f"." + num.rjust(2, '0'))
-
-
-############################################################
-
-elif m in m31 and num <= 31:
-    if num ==31:
-        m = str(m)
-
-        num0 = int(num) - 1
-        num0 = str(num0)
-
-        num = 1
-        num = str(num)
-
-        m1 = int(m) + 1
-        m1 = str(m1)
-
-        print(m.rjust(2, '0') + f"." + num0.rjust(2, "0"), m1.rjust(2, '0') + f"." + num.rjust(2, '0'))
-
-    elif m in m31 and num <=30 and num >=2:
-
-        m = str(m)
-
-        num0 = int(num) -1
-        num0 = str(num0)
-
-        num = int(num)+1
-        num = str(num)
-
-        m1 = int(m)
-        m1 = str(m1)
-
-        print(m.rjust(2, '0') + f"." + num0.rjust(2, "0"), m1.rjust(2, '0') + f"." + num.rjust(2, '0'))
-
-
-    elif m in m31 and m ==1 and num ==1:
-        m  = 12
-        m = str(m)
-
-        num0 = 31
-        num0 = str(num0)
-
-        num = int(num)+1
-        num = str(num)
-
-        m1 = 1
-        m1 = str(m1)
-
-        print(m.rjust(2, '0') + f"." + num0.rjust(2, "0"), m1.rjust(2, '0') + f"." + num.rjust(2, '0'))
-
-
-
-
-elif m in m30 and num <=30:
-    if num ==30:
-        m = str(m)
-
-        num0 = int(num) - 1
-        num0 = str(num0)
-
-        num = 1
-        num = str(num)
-
-        m1 = int(m) + 1
-        m1 = str(m1)
-
-        print(m.rjust(2, '0') + f"." + num0.rjust(2, "0"), m1.rjust(2, '0') + f"." + num.rjust(2, '0'))
-
-    elif m in m30 and num <=30 and num >=2:
-
-        m = str(m)
-
-        num0 = int(num) -1
-        num0 = str(num0)
-
-        num = int(num)+1
-        num = str(num)
-
-        m1 = int(m)
-        m1 = str(m1)
-
-        print(m.rjust(2, '0') + f"." + num0.rjust(2, "0"), m1.rjust(2, '0') + f"." + num.rjust(2, '0'))
-
-
-    elif m in m30 and  num ==1:
-        m  =int(m)-1
-        m = str(m)
-
-        num0 = 31
-        num0 = str(num0)
-
-        num = int(num)+1
-        num = str(num)
-
-        m1 = int(m)+1
-        m1 = str(m1)
-
-        print(m.rjust(2, '0') + f"." + num0.rjust(2, "0"), m1.rjust(2, '0') + f"." + num.rjust(2, '0'))
-
-else:
-    print("Вводите верно дату")
-    print(f'{m - 1:02}.{t[m - 2]:02} {m:02}.{num:02}')
+print(out_filter)
--- 00 Базовый курс/051 Функция filter/01 Функция filter.py
+++ 00 Базовый курс/051 Функция filter/01 Функция filter.py
@@ -26,3 +26,28 @@ print(b)
 a ='sdfsf4234sdfdsf'
 b =list(filter(str.isdigit,a)) 
 print(b)
+
+
+
+"""
+    Программа Python для фильтрации нечетных чисел
+    в списке, используя функцию filter()
+"""
+
+# список чисел
+numbers = [1, 2, 4, 5, 7, 8, 10, 11]
+
+# функция, которая проверяет числа
+def ne_chet(in_num):
+    if(in_num % 2) == 0:
+        return True
+    else:
+        return False
+
+# # Применение filter() для удаления нечетных чисел
+out_filter = list(filter(ne_chet,numbers))
+
+print(out_filter)
+
+znechet =list(filter(lambda x:x %2==1,numbers))
+print(znechet)
