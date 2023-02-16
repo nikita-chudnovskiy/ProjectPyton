70c468364e9f6d89ef4f2ee19ad0ea3c31a6181f
nikita-chudnovskiy <nikita.chudnovskiy@mail.ru>
2023-02-16 11:18:28 +0300

    add

--- 00 Базовый курс/022 Цикл while. Инструкции break, continue, else/01 break continue else.py
+++ 00 Базовый курс/022 Цикл while. Инструкции break, continue, else/01 break continue else.py
@@ -26,3 +26,13 @@ while i <len(str):
 
     print(str[i])
     i += 1
+
+i = 0
+str1 = 'javatpoint'
+
+while i < len(str1):
+    if str1[i] == 't':
+        i += 1
+        break
+    print('Current Letter :', str1[i])
+    i += 1
