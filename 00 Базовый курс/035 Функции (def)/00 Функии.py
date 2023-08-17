756c5e8356227e4e4f6940178ec2959d1eaad604
n.chudnovskiy <nikita.chudnovskiy@mail.ru>
2023-08-17 12:54:32 +0300

    add

--- 00 Базовый курс/035 Функции (def)/00 Функии.py
+++ 00 Базовый курс/035 Функции (def)/00 Функии.py
@@ -7,3 +7,10 @@ def square(x,y,z): # параметры функции
 s =square(3,3,3) # ( то что в скобках это аргументы функции)
 
 print(f'площадь треугольника = {s}')
+
+
+def param(x,y):
+    return x*y
+
+s =param(3,5)
+print(s)
