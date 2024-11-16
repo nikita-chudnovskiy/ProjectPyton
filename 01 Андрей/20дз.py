92b49803fd043d021783a281cef55efdc6a4da45
n.chudnovskiy <nikita.chudnovskiy@mail.ru>
2024-11-16 11:42:03 +0300

    `1`

---
+++ 01 Андрей/20дз.py
@@ -0,0 +1,39 @@
+# позиция = 0
+# нашли = False
+# s = []
+# print('Введите фамилии 6 людей в очереди')
+# for i in range(6):
+#     i = i + 1
+#     a = input()
+#     s.append(a)
+# print('Введите фамилию')
+# b = input()
+# print('Позиция в очереди:',i)
+# for i in s:
+#     if i == b:
+#         нашли = True
+#         break
+#     else:
+#         позиция = позиция+1
+# if b != i:
+#     print('Такого человека нет в очереди')
+# else:
+#     print(позиция)
+
+# s=[]
+# while True:
+#     a = input()
+#     if a == 'стоп':
+#         break
+#     s.append(a)
+# g = set(s)
+# print("Количество различных слов:",len(g))
+
+# s = []
+# while True:
+#     a = input()
+#     if a == 'стоп':
+#         break
+#     if a not in s:
+#         s.append(a)
+# print('Количество различных слов:',len(s))
---
+++ 01 Андрей/21занятие.py
@@ -0,0 +1,7 @@
+a = []
+for i in range(5):
+    b = input()
+    a.append(b)
+for i in a:
+    if i == 'cat':
+        print('Yes')
