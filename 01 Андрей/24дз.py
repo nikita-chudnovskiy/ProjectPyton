b43b15e0f6c92ce6f6314d751395c11f1afa11d6
n.chudnovskiy <nikita.chudnovskiy@mail.ru>
2024-12-01 17:00:01 +0300

    12

--- 01 Андрей/24дз.py
+++ 01 Андрей/24дз.py
@@ -1,4 +1,25 @@
-a = [1,2,3,4,5,6,7]
-print('Введите shift')
-b = int(input())
 
+# a = [1,2,3,4,5,6,7]
+# print('Введите shift')
+# b = int(input())
+# for i in range(b):
+#     last = [a[6]]
+#     right = a[:5+1]
+#     a = last+right
+# print(a)
+
+b = []
+for i in range(6):
+    a = int(input())
+    b.append(a)
+maxb = max(b)
+mind = min(b)
+f = []
+for a in b:
+    if a == maxb:
+        f.append(mind)
+    if a == mind:
+        f.append(maxb)
+    if a != maxb and a != mind:
+        f.append(a)
+print(f)
---
+++ 01 Андрей/25занятие.py
@@ -0,0 +1,6 @@
+a = [1,3,4,7,8,10]
+b = []
+for i in a:
+    if i > 5:
+        b.append(i)
+print(b)
