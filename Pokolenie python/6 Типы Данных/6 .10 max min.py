a68897d5b8c3c775fd4d79d23362a6e064e8a510
nikita <nikita.chudnovskiy@mail.ru>
2024-12-16 15:39:53 +0300

    1

---
+++ Pokolenie python/6 Типы Данных/6 .10 max min.py
@@ -0,0 +1,8 @@
+num1: int = int(input())
+num2: int = int(input())
+num3: int = int(input())
+num4: int = int(input())
+num5: int = int(input())
+
+print('Наименьшее число =', min(num1, num2, num3, num4, num5))
+print('Наибольшее число =', max(num1, num2, num3, num4, num5))
