8c1d0882494613ac28294bd8e79f17189d3aa468
nikita <nikita.chudnovskiy@mail.ru>
2024-12-18 14:28:38 +0300

    message

---
+++ Pokolenie python/6 Типы Данных/6.13 Сумма модулей.py
@@ -0,0 +1,3 @@
+# 5 Действительных если будет -1 то все посчитает в + что нам и нужно
+a, b, c, d, e = abs(float(input())), abs(float(input())), abs(float(input())), abs(float(input())), abs(float(input()))
+print(a + b + c + d + e)
---
+++ Test/5.py
@@ -0,0 +1,6 @@
+
+import pytest
+import sys
+from _pytest.config import get_plugin_manager
+import warnings
+print('hello')
