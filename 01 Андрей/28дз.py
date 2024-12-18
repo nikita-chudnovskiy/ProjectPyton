a92535004782007258272299f028de8275ed44b2
n.chudnovskiy <nikita.chudnovskiy@mail.ru>
2024-12-18 14:49:05 +0300

    12

---
+++ 01 Андрей/28дз.py
@@ -0,0 +1,23 @@
+# print('Введите 5 чисел')
+# a = []
+# b = []
+# for i in range(5):
+#     c = int(input())
+#     a.append(c)
+#     if c < 5:
+#         f = c*2
+#         b.append(f)
+#     if c >=5:
+#         b.append(c)
+# print('Исходный список:',a)
+# print('модифицированный список:',b)
+
+# a = []
+# while True:
+#     print('Введите имя')
+#     b = input()
+#     if b not in a:
+#         a.append(b)
+#     if b in a:
+#         pass
+#     print(a)
---
+++ 01 Андрей/29занятие.py
@@ -0,0 +1,29 @@
+# a = [1,3,5,4,6,8,9]
+# b = []
+# for d in a:
+#     if d > 4:
+#         b.append(d)
+#     if d < 4:
+#         pass
+# print(b)
+
+# g = [1,3,2,4,5,6,4]
+# a = []
+# for b in g:
+#     if b == 4:
+#         a.append(500)
+#     if b != 4:
+#         a.append(b)
+# print(a)
+
+# a = int(input())
+# b = int(input())
+# s = int(input())
+# print(min(a,b,s))
+
+f = 0
+a = [10, 4, 7, 2, 3]
+for b in a:
+    if b < 5:
+        f = f + 1
+print(f)
